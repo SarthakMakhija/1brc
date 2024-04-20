41d903a0959e2151d015a09d1c850e195175d272
Sarthak <sarthak.makhija@gmail.com>
2024-04-20 23:03:31 +0530

    Sarthak | Introduces zero copy string in bytes, add a separate profile

--- utils/bytes.go
+++ utils/bytes.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"unsafe"
 )
 
 var separator = byte(';')
@@ -9,14 +10,26 @@ var separator = byte(';')
 var ErrInvalidLineFormat = errors.New("invalid line format")
 
 func SplitIntoStationNameAndTemperature(line []byte) (string, string, error) {
+	stationName := func(endIndex int) []byte {
+		station := make([]byte, endIndex)
+		copy(station, line[:endIndex])
+		return station
+	}
+
+	temperature := func(startIndex int) []byte {
+		temperature := make([]byte, len(line)-startIndex)
+		copy(temperature, line[startIndex:])
+		return temperature
+	}
+
 	for index := 0; index < len(line); index++ {
 		if line[index] == separator {
-			return toString(line[:index]), toString(line[index+1:]), nil
+			return zeroCopyString(stationName(index)), zeroCopyString(temperature(index + 1)), nil
 		}
 	}
 	return "", "", ErrInvalidLineFormat
 }
 
-func toString(slice []byte) string {
-	return string(slice)
+func zeroCopyString(slice []byte) string {
+	return *(*string)(unsafe.Pointer(&slice))
 }
