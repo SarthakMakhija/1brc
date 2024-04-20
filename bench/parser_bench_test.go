4a9a93122cc9f507fd6fc3a9cce4537ed78dc8ac
Sarthak <sarthak.makhija@gmail.com>
2024-04-20 16:11:39 +0530

    Sarthak | Adds benchmark results

--- bench/parser_bench_test.go
+++ bench/parser_bench_test.go
@@ -7,6 +7,35 @@ import (
 	"testing"
 )
 
+/*
+**
+go test -run none -bench . -benchtime 10s -count 7 | tee bench_44k.txt
+benchstat bench_44k.txt
+
+goos: linux
+goarch: amd64
+pkg: 1brc/bench
+cpu: 13th Gen Intel(R) Core(TM) i7-1360P
+BenchmarkParseWeatherStations44K-16    	 6535921	      1743 ns/op
+BenchmarkParseWeatherStations44K-16    	 6775304	      1698 ns/op
+BenchmarkParseWeatherStations44K-16    	 6924301	      1688 ns/op
+BenchmarkParseWeatherStations44K-16    	 6917014	      1685 ns/op
+BenchmarkParseWeatherStations44K-16    	 6888340	      1692 ns/op
+BenchmarkParseWeatherStations44K-16    	 6947594	      1689 ns/op
+BenchmarkParseWeatherStations44K-16    	 6912704	      1697 ns/op
+
+benchstat bench_44k.txt
+goos: linux
+goarch: amd64
+pkg: 1brc/bench
+cpu: 13th Gen Intel(R) Core(TM) i7-1360P
+
+	│ bench_44k.txt │
+	│    sec/op     │
+
+ParseWeatherStations44K-16     1.692µ ± 3%
+**
+*/
 func BenchmarkParseWeatherStations44K(b *testing.B) {
 	file, err := os.Open("../fixture/44K_weather_stations.csv")
 	if err != nil {
