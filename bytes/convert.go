98d3bc1057e4bfcfa77a1686c503ec80c774977a
Sarthak <sarthak.makhija@gmail.com>
2024-04-21 12:20:40 +0530

    Sarthak | Adds a custom ToFloat64

---
+++ bytes/convert.go
@@ -0,0 +1,64 @@
+package bytes
+
+import (
+	"errors"
+	"fmt"
+)
+
+var errFloatParse = errors.New("cannot parse float64")
+
+var float64pow10 = [...]float64{
+	1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12, 1e13, 1e14, 1e15, 1e16,
+}
+
+func ToFloat64(input string) (float64, error) {
+	return asFloat64(input)
+}
+
+func asFloat64(input string) (float64, error) {
+	var asFloat float64
+	minus := input[0] == '-'
+
+	wholeValue, currentIndex := integerPart(input)
+	if input[currentIndex] == '.' {
+		currentIndex++
+		dotIndex := currentIndex
+
+		for currentIndex < uint(len(input)) {
+			if input[currentIndex] >= '0' && input[currentIndex] <= '9' {
+				wholeValue = wholeValue*10 + uint64(input[currentIndex]-'0')
+				currentIndex++
+				continue
+			}
+			break
+		}
+
+		asFloat = float64(wholeValue) / float64pow10[currentIndex-dotIndex]
+		if currentIndex >= uint(len(input)) {
+			if minus {
+				asFloat = -asFloat
+			}
+			return asFloat, nil
+		}
+	}
+	return 0, fmt.Errorf("%v, input %s", errFloatParse, input)
+}
+
+func integerPart(input string) (uint64, uint) {
+	currentIndex := uint(0)
+	minus := input[0] == '-'
+	if minus {
+		currentIndex++
+	}
+
+	wholeValue := uint64(0)
+	for currentIndex < uint(len(input)) {
+		if input[currentIndex] >= '0' && input[currentIndex] <= '9' {
+			wholeValue = wholeValue*10 + uint64(input[currentIndex]-'0')
+			currentIndex++
+			continue
+		}
+		break
+	}
+	return wholeValue, currentIndex
+}
---
+++ bytes/convert_test.go
@@ -0,0 +1,26 @@
+package bytes
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestToFloat64(t *testing.T) {
+	inputTable := []struct {
+		input    string
+		expected float64
+	}{
+		{input: "10.43", expected: 10.43},
+		{input: "19.4333", expected: 19.4333},
+		{input: "-34.5997", expected: -34.5997},
+		{input: "40.6943", expected: 40.6943},
+		{input: "40.6", expected: 40.6},
+		{input: "-40.1", expected: -40.1},
+	}
+
+	for _, row := range inputTable {
+		result, err := ToFloat64(row.input)
+		assert.Nil(t, err)
+		assert.Equal(t, row.expected, result)
+	}
+}
