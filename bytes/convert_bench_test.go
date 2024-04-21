3f737b8834cbe0d4a7e074d86c3adcbd1db13935
Sarthak <sarthak.makhija@gmail.com>
2024-04-21 20:14:37 +0530

    Sarthak | Adds a profile for change in convert to handle just one fractional digit

--- bytes/convert_bench_test.go
+++ bytes/convert_bench_test.go
@@ -57,6 +57,22 @@ BenchmarkConvertToFloat64-16    	13323096	      1758 ns/op
 BenchmarkConvertToFloat64-16    	13637918	      1749 ns/op
 BenchmarkConvertToFloat64-16    	13335798	      1757 ns/op
 */
+
+/*
+After changing convert to handle only one fractional digit.
+
+go test -run none -bench BenchmarkConvertToFloat64 -benchtime 20s -count 6 -cpuprofile convert_to_float_64_cpu_handle_one_fractional_digit.out
+goos: linux
+goarch: amd64
+pkg: 1brc/bytes
+cpu: 13th Gen Intel(R) Core(TM) i7-1360P
+BenchmarkConvertToFloat64-16    	36951853	      1159 ns/op
+BenchmarkConvertToFloat64-16    	15636702	      1539 ns/op
+BenchmarkConvertToFloat64-16    	15605932	      1509 ns/op
+BenchmarkConvertToFloat64-16    	15333734	      1556 ns/op
+BenchmarkConvertToFloat64-16    	15502268	      1550 ns/op
+BenchmarkConvertToFloat64-16    	15492415	      1552 ns/op
+*/
 func BenchmarkConvertToFloat64(b *testing.B) {
 	var localSink float64
 	input := []byte("-10.443")
