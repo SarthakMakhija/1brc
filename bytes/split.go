cb578500ffd5d69c750ea7e130e46866d0cb8273
Sarthak <sarthak.makhija@gmail.com>
2024-05-04 16:26:17 +0530

    Sarthak | Changes SplitIntoStationNameAndTemperature and replaces the loop with indexing

--- bytes/split.go
+++ bytes/split.go
@@ -19,28 +19,42 @@ var ErrInvalidLineFormat = errors.New("invalid line format")
 func SplitIntoStationNameAndTemperature(line []byte) ([]byte, Temperature, error) {
 	lineLength := len(line)
 	//prevent bound checks
-	if lineLength < 3 {
+	if lineLength < 5 {
 		return nil, 0, ErrInvalidLineFormat
 	}
 
 	fractionalValue := int16(line[lineLength-1] - '0') //lineLength-1 represents the fractional digit.
 	integerValue := int16(line[lineLength-3] - '0')    //lineLength-3 represents the lowest position temperature digit.
 
-	for index := lineLength - 4; index >= 0; index-- {
-		switch ch := line[index]; ch {
-		case minusSign:
-			eligibleTemperature := integerValue*multiplier + (fractionalValue)
-			eligibleTemperature = ^eligibleTemperature + 1
-			if index-1 >= 0 { //prevent IsSliceInBounds checks
-				return line[:index-1], eligibleTemperature, nil
-			}
-			return nil, -1, ErrInvalidLineFormat
-		case separator:
-			eligibleTemperature := integerValue*multiplier + (fractionalValue)
-			return line[:index], eligibleTemperature, nil
-		default:
-			integerValue = integerValue + int16(ch-('0'))*multiplier
+	ch := line[lineLength-4]
+	switch ch {
+	case minusSign:
+		eligibleTemperature := integerValue*multiplier + (fractionalValue)
+		eligibleTemperature = ^eligibleTemperature + 1
+		if lineLength-5 >= 0 { //prevent IsSliceInBounds checks
+			return line[:lineLength-5], eligibleTemperature, nil
 		}
+		return nil, -1, ErrInvalidLineFormat
+	case separator:
+		eligibleTemperature := integerValue*multiplier + (fractionalValue)
+		return line[:lineLength-4], eligibleTemperature, nil
+	default:
+		integerValue = integerValue + int16(ch-('0'))*multiplier
 	}
-	return nil, -1, ErrInvalidLineFormat
+
+	ch = line[lineLength-5]
+	switch ch {
+	case minusSign:
+		eligibleTemperature := integerValue*multiplier + (fractionalValue)
+		eligibleTemperature = ^eligibleTemperature + 1
+		if lineLength-6 >= 0 { //prevent IsSliceInBounds checks
+			return line[:lineLength-6], eligibleTemperature, nil
+		}
+		return nil, -1, ErrInvalidLineFormat
+	case separator:
+		eligibleTemperature := integerValue*multiplier + (fractionalValue)
+		return line[:lineLength-5], eligibleTemperature, nil
+	default:
+		return nil, -1, ErrInvalidLineFormat
+	}
 }
--- bytes/split_test.go
+++ bytes/split_test.go
@@ -15,12 +15,12 @@ func TestSplitIntoStationNameAndTemperature(t *testing.T) {
 }
 
 func TestSplitIntoStationNameAndNegativeTemperature(t *testing.T) {
-	line := []byte("Odesa;-10.4")
+	line := []byte("Odesa;-99.9")
 	stationName, temperature, err := SplitIntoStationNameAndTemperature(line)
 
 	assert.Nil(t, err)
 	assert.Equal(t, "Odesa", string(stationName))
-	assert.Equal(t, Temperature(-104), temperature)
+	assert.Equal(t, Temperature(-999), temperature)
 }
 
 func TestSplitIntoStationNameAndNegativeTemperatureWithStationNameHavingSpecialCharacters(t *testing.T) {
@@ -30,4 +30,22 @@ func TestSplitIntoStationNameAndNegativeTemperatureWithStationNameHavingSpecialC
 	assert.Nil(t, err)
 	assert.Equal(t, "São Paulo", string(stationName))
 	assert.Equal(t, Temperature(-104), temperature)
+}
+
+func TestSplitIntoStationNameAndPositiveTemperatureLessThan10(t *testing.T) {
+	line := []byte("São Paulo;1.4")
+	stationName, temperature, err := SplitIntoStationNameAndTemperature(line)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "São Paulo", string(stationName))
+	assert.Equal(t, Temperature(14), temperature)
+}
+
+func TestSplitIntoStationNameAndNegativeTemperatureLessThan10(t *testing.T) {
+	line := []byte("São Paulo;-1.4")
+	stationName, temperature, err := SplitIntoStationNameAndTemperature(line)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "São Paulo", string(stationName))
+	assert.Equal(t, Temperature(-14), temperature)
 }
